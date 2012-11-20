d2ddbe481a327b4aef71c968aa8ea49d3211070c
Cristina Hanes <clhanes@hotmail.com>
2012-11-19 21:01:19 -0800

    Issue #1845392 by cristinawithout: Update js with closest()

--- js/autoupload.js
+++ js/autoupload.js
@@ -7,7 +7,7 @@
     attach: function(context, settings) {
       $('.form-item input.form-submit[value=Upload]', context).hide();
       $('.form-item input.form-file', context).change(function() {
-        $parent = $(this).parents('.form-item');
+        $parent = $(this).closest('.form-item');
         
         //setTimeout to allow for validation
         //would prefer an event, but there isn't one
