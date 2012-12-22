7f5180f4fa3eb32ec939f1105556a866b0a3fc1f
Cristina Hanes <clhanes@hotmail.com>
2012-12-21 18:03:15 -0800

    Issue #1843204: Add media module support

--- js/autoupload.js
+++ js/autoupload.js
@@ -4,19 +4,80 @@
 
 (function ($) {
   Drupal.behaviors.autoUpload = {
-    attach: function(context, settings) {
-      $('.form-item input.form-submit[value=Upload]', context).hide();
-      $('.form-item input.form-file', context).change(function() {
-        $parent = $(this).closest('.form-item');
+    attach: function (context, settings) {
+      /*
+       * selectors is an associative array. for more info, see
+       * _autoupload_get_predefined in autoupload.admin.inc
+       */
+      var selectors = settings.autoupload.selectors;
 
-        //setTimeout to allow for validation
-        //would prefer an event, but there isn't one
-        setTimeout(function() {
-          if(!$('.error', $parent).length) {
-            $('input.form-submit[value=Upload]', $parent).mousedown();
-          }
-        }, 100);
+      $.each(selectors, function (index, value) {
+        var fs_context = value['context'];
+        $(fs_context, context).each(function () {
+          var $wrapper = $(this);
+          var file_input = value['file_input'];
+          var file_event = value['file_event'];
+          var submit_input = value['submit_input'];
+          var submit_event = value['submit_event'];
+          var error = value['error'];
+          var error_remove = value['error_remove'];
+
+          /*
+           * add CSS to the document instead of .hide(), because the element
+           * may be added dynamically after this point. cannot delegate "load"
+           */
+          $('head').append($('<' + 'style media="all" type="text/css"' + '>' + fs_context + ' ' + submit_input + ' { display:none; }<' + '/style' + '>'));
+
+          $wrapper.delegate(file_input, file_event, function (e) {
+            if (!error || $(this).hasClass('autoupload-processed')) {
+              //no error check or error check has been performed
+
+              setTimeout(function () {
+                //allow for validation. (prefer event, but there isn't one)
+                if (!$wrapper.find(error).length > 0) {
+                  $wrapper.find(submit_input).trigger(submit_event);
+                }
+              }, 100);
+            }
+            else if (error) {
+              //perform error check
+              e.preventDefault();
+
+              if (!error_remove) {
+                //error removal is handled already. no need to do anything
+              }
+              if (error_remove == 'id') {
+                var elem_id = error.substr(error.lastIndexOf('#') + 1);
+                if (!/[: .[]+/.test(elem_class)) {
+                  $wrapper.find(error).removeAttr(elem_id);
+                }
+              }
+              else if (error_remove == 'class') {
+                var elem_class = error.substr(error.lastIndexOf('.') + 1);
+                if (!/[: #[]+/.test(elem_class)) {
+                  $wrapper.find(error).removeClass(elem_class);
+                }
+              }
+              else if (error_remove == 'element') {
+                $wrapper.find(error).remove();
+              }
+
+              $(this).addClass('autoupload-processed');
+              $(this).trigger(file_event);
+            }
+          });
+        });
       });
+
+      /*
+       * Media module's cancel button is added after load, so delay for a
+       * moment then resize the media browser so the cancel button is visible
+       */
+      if (settings.autoupload.predefined.media && Drupal && Drupal.media && Drupal.media.browser && typeof Drupal.media.browser.resizeIframe == 'function') {
+        setTimeout(function () {
+          Drupal.media.browser.resizeIframe();
+        }, 100);
+      }
     }
   };
 })(jQuery);
